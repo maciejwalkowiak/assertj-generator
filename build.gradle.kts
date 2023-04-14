4f847e203c192bcb181b89347e2b6bd1d4c6a446
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2023-04-14 12:33:24 +0200

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ java {
 }
 
 plugins {
-    id("java")
+    `java-library`
 }
 
 group = "com.maciejwalkowiak.assertj.generator"
@@ -16,6 +16,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.assertj:assertj-core:3.23.1")
+    implementation("com.squareup:javapoet:1.13.0")
+    implementation("org.reflections:reflections:0.10.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
